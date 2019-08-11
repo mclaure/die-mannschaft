0be0d4648352924df3b3d01e533cd1b9e851598e
mclaure <mclaure@gmail.com>
2019-08-11 16:16:42 -0400

    Adding support to MySQL

---
+++ src/api/controllers/MySQLController.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var config = require('./settings');
+var mysql = require("mysql");
+
+//Creates DB connection
+var mysqlConn = mysql.createConnection({
+    host     : config.mysql.host,
+    user     : config.mysql.user,
+    password : config.mysql.password,
+    database : config.mysql.database
+  });
+
+exports.list_all_countries = (req, res, next) => {
+    let sql = "SELECT id \
+                    ,name \
+               FROM Country;";
+    var params = [];
+
+    mysqlConn.query(sql, function (error, rows, fields) {
+		if (error) throw error;
+        return res.json({"countries":rows});
+    });
+};
+
+exports.list_all_teams = (req, res, next) => {
+    let sql = 'SELECT id \
+                     ,team_long_name AS full_name \
+                     ,team_short_name AS short_name \
+               FROM Team;';
+    var params = [];
+
+    mysqlConn.query(sql, function (error, rows, fields) {
+		if (error) throw error;
+        return res.json({"teams":rows});
+    });
+};
+
+exports.list_all_leagues = (req, res, next) => {
+    let sql = 'SELECT id \
+                     ,name \
+               FROM League;';
+    var params = [];
+
+    mysqlConn.query(sql, function (error, rows, fields) {
+		if (error) throw error;
+        return res.json({"leagues":rows});
+    });
+};
+
+exports.get_player_info = (req, res, next) => {
+    let sql = "SELECT id AS id \
+                     ,player_name AS name \
+                     ,DATE('now')-birthday AS age \
+                     ,weight \
+                     ,height \
+               FROM Player WHERE id = ?;";
+    let params = [req.params.id];
+
+    mysqlConn.query(sql, params, function (error, rows, fields) {
+        if (error) throw error;
+        else {
+            if(rows) return res.json({ "player" : rows });
+            return res.json({ "player" : "Not Found" });
+        }  
+    });
+};
+
+exports.get_summary_per_season = (req, res, next) => {
+    let sql = "SELECT ct.name AS country \
+                     ,le.name AS league \
+                     ,mtc.season \
+                     ,mtc.stage \
+                     ,COUNT(*) AS total_matchs \
+                     ,SUM(home_team_goal + away_team_goal) AS total_goals \
+                     ,ROUND(AVG(home_team_goal + away_team_goal),0) AS average_goal_per_match \
+               FROM soccer.Match      mtc \
+               INNER JOIN soccer.Country ct ON (mtc.country_id = ct.id) \
+               INNER JOIN soccer.League  le ON (mtc.league_id = le.id) \
+               WHERE mtc.season LIKE ? \
+               GROUP BY  ct.name \
+                        ,le.name \
+                        ,mtc.season \
+                        ,mtc.stage;";
+    let params = "%" + [req.params.season] + "%";
+
+    mysqlConn.query(sql, params, function (error, rows, fields) {
+        if (error) throw error;
+        else {
+            if(rows) return res.json({ "seasons" : rows });
+            return res.json({ "seasons" : "Not Found" });
+        }  
+    });
+};
--- src/api/routes/route.js
+++ src/api/routes/route.js
@@ -2,20 +2,38 @@
 
 module.exports = function(app) {
 
-  var apiController = require('../controllers/controller');
+  var sqlite3 = require('../controllers/SQLiteController');
+  var mysql = require('../controllers/MySQLController');
 
-  app.route('/api/countries/')
-    .get(apiController.list_all_countries);
+  //SQLite3
+  app.route('/api/sqlite/countries/')
+    .get(sqlite3.list_all_countries);
 
-  app.route('/api/teams/')
-    .get(apiController.list_all_teams);    
+  app.route('/api/sqlite/teams/')
+    .get(sqlite3.list_all_teams);    
 
-  app.route('/api/leagues/')
-    .get(apiController.list_all_leagues);       
+  app.route('/api/sqlite/leagues/')
+    .get(sqlite3.list_all_leagues);       
 
-  app.route('/api/player/:id')
-    .get(apiController.get_player_info);          
+  app.route('/api/sqlite/player/:id')
+    .get(sqlite3.get_player_info);          
 
-  app.route('/api/summary/:season')
-    .get(apiController.get_summary_per_season);        
+  app.route('/api/sqlite/summary/:season')
+    .get(sqlite3.get_summary_per_season); 
+  
+  //MySQL
+  app.route('/api/mysql/countries/')
+  .get(mysql.list_all_countries);
+
+  app.route('/api/mysql/teams/')
+  .get(mysql.list_all_teams);  
+
+  app.route('/api/mysql/leagues/')
+    .get(mysql.list_all_leagues);       
+
+  app.route('/api/mysql/player/:id')
+    .get(mysql.get_player_info);   
+    
+  app.route('/api/mysql/summary/:season')
+    .get(mysql.get_summary_per_season);     
 };
